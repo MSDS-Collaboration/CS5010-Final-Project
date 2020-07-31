c8f20e8fc24fb7b7dc9598b2d34718d07db8c859
Nikki Aaron <31260131+NA-Dev@users.noreply.github.com>
2020-07-31 13:06:00 -0400

    Change modified data to utf8, add unit tests

--- grape_list_parser.py
+++ grape_list_parser.py
@@ -1,33 +1,49 @@
-
-
 from csv import writer
 from csv import reader
 import re
 
 print('>>> Starting')
 
-with open('grape_list.csv', 'r') as read_obj, \
-    open('grape_list_parsed.csv', 'w', newline='') as write_obj:
-    # Create a csv.reader object from the input file object
-    csv_reader = reader(read_obj, delimiter=';')
-    # Create a csv.writer object from the output file object
-    csv_writer = writer(write_obj)
-    # Read each row of the input csv file as list
-    next(csv_reader) # Skip first line
+# Function to split a comma or forward slash separated string to a list
+def splitNames(namesStr):
+    nameLst = re.split(r",|\/", namesStr, flags=re.IGNORECASE)
+    nameLst = [name.strip() for name in nameLst]
+    return nameLst
 
-    csv_writer.writerow(['Name', 'Type'])
-    for row in csv_reader:
-        color = row[2]
-        # Append the default text in the row / list
-        for phrase in re.split(r",|\/", row[0], flags=re.IGNORECASE):
-            if len(phrase):
-                phrase = re.sub(r"[\(\[].*?[\)\]]", "", phrase).strip()
-                csv_writer.writerow([phrase, color])
+# Function to remove parenthesis or brackets and their contents
+def removeComments(name):
+    return re.sub(r"[\(\[].*?[\)\]]", "", name).strip()
 
-        for phrase in re.split(r",|\/", row[1], flags=re.IGNORECASE):
-            if len(phrase):
-                phrase = re.sub(r"[\(\[].*?[\)\]]", "", phrase).strip()
-                csv_writer.writerow([phrase, color])
+def parseGrapeList():
+    print('>>> Starting')
+    with open('grape_list.csv', 'r') as read_obj, \
+        open('grape_list_parsed.csv', 'w', newline='') as write_obj:
 
+        # Create a csv.reader object from the input file object
+        csv_reader = reader(read_obj, delimiter=';')
+        # Create a csv.writer object from the output file object
+        csv_writer = writer(write_obj)
 
-print('>>> Finished')
+        # Skip first header row
+        next(csv_reader)
+
+        # Write output file headers
+        csv_writer.writerow(['Name', 'Type'])
+
+        # Read each row of the input file as list
+        for row in csv_reader:
+            color = row[2]
+            for phrase in splitNames(row[0]):
+                if len(phrase):
+                    phrase = removeComments(phrase)
+                    csv_writer.writerow([phrase, color])
+
+            for phrase in splitNames(row[1]):
+                if len(phrase):
+                    phrase = removeComments(phrase)
+                    csv_writer.writerow([phrase, color])
+
+    print('>>> Finished')
+
+# Uncomment this and run to parse the grape_list.csv file
+# parseGrapeList()
